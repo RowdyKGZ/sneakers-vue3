8fdc37169bbf577d7d8d372d9fed036559461dab
RowdyKGZ <rowdykg@gmail.com>
2024-01-26 18:25:07 +0600

    Card

---
+++ src/components/Card.vue
@@ -0,0 +1,36 @@
+<script setup>
+defineProps({
+  title: String,
+  imageUrl: String,
+  price: Number,
+  isFavorite: Boolean,
+  isAdded: Boolean,
+  onCLickAdd: Function,
+  onCLickFaorite: Function
+})
+</script>
+
+<template>
+  <div
+    class="relative bg-white m-10 rounded-3xl p-8 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl"
+  >
+    <img
+      @click="onCLickFaorite"
+      :src="isFavorite ? `/like-2.svg` : `/like-1.svg`"
+      alt="Like 1"
+      class="absolute top-8 left-8"
+    />
+
+    <img :src="imageUrl" alt="Sneaker" />
+    <p class="mt-2">{{ title }}</p>
+
+    <div class="flex justify-between mt-5">
+      <div class="flex flex-col">
+        <span class="text-slate-500">Price</span>
+        <b>{{ price }}$</b>
+      </div>
+
+      <img @click="onCLickAdd" :src="isAdded ? `/checked.svg` : `/plus.svg`" alt="add sneakers" />
+    </div>
+  </div>
+</template>
---
+++ src/components/CardList.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Card from './Card.vue'
+
+const onClickAdd = () => {
+  alert('Hello')
+}
+</script>
+
+<template>
+  <div class="grid grid-cols-3">
+    <Card
+      image-url="/sneakers/sneakers-1.jpg"
+      title="Men's Nike Sneakers"
+      :price="100"
+      is-favorite
+      :onCLickAdd="onClickAdd"
+    />
+    <Card image-url="/sneakers/sneakers-2.jpg" title="Men's Nike Sneakers" :price="350" />
+    <Card image-url="/sneakers/sneakers-3.jpg" title="Men's Nike Sneakers" :price="400" is-added />
+  </div>
+</template>
