b59ef24c21a5b7faae75001f27371b2e706ce031
RowdyKGZ <rowdykg@gmail.com>
2024-01-27 15:20:39 +0600

    Cart drawer

---
+++ src/components/CartItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex items-center border border-slate-100 p-4 rounded-xl gap-4">
+    <img src="/sneakers/sneakers-1.jpg" alt="sneaker" class="w-16 h-16" />
+
+    <div class="flex flex-col">
+      <p>Sneakers Nike Air Max 270</p>
+      <div class="flex justify-between mt-2">
+        <b>100$</b>
+        <img
+          src="/close.svg"
+          alt="closed"
+          class="opacity-50 hover:opacity-100 cursor-pointer transition"
+        />
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/components/Drawer.vue
@@ -0,0 +1,36 @@
+<script setup>
+import DrawerHead from './DrawerHead.vue'
+
+import CardListItem from './CartListItem.vue'
+</script>
+
+<template>
+  <div class="fixed top-0 left-0 h-full w-full bg-black z-10 opacity-70"></div>
+
+  <div class="bg-white w-96 h-full fixed right-0 top-0 z-20 p-8">
+    <DrawerHead />
+
+    <CardListItem />
+
+    <div class="flex flex-col gap-3 my-4">
+      <div class="flex gap-2">
+        <span>Outcome</span>
+        <div class="flex-1 border-b border-dashed"></div>
+        <b>500$</b>
+      </div>
+
+      <div class="flex gap-2">
+        <span>Tax 5%</span>
+        <div class="flex-1 border-b border-dashed"></div>
+        <b>5$</b>
+      </div>
+
+      <button
+        disabled
+        class="bg-lime-500 w-full rounded-xl py-3 text-white hover:bg-lime-600 transition cursor-pointer active:bg-lime-700 disabled:bg-slate-300"
+      >
+        Place an order
+      </button>
+    </div>
+  </div>
+</template>
